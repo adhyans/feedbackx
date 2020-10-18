ba9c2c165395dd09bdf804b79418084362c1d593
Adhyan <adhyan_srivastava95@yahoo.com>
2020-10-18 13:47:28 +0530

    initia setup done

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./client/src/index",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name]-[contenthash].js",
+    chunkFilename: "[name]-[chunkhash].js",
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "async",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "client/index.html",
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
+};
